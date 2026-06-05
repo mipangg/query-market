61e7ddda6efb85b99d32e67d3f37741e4c0a6bcd
mipangg <morganjeong119@gmail.com>
2026-06-06 08:02:15 +0900

    docs: 리드미 작성

--- src/main/java/io/mipangg/querymarket/domain/product/controller/ProductController.java
+++ src/main/java/io/mipangg/querymarket/domain/product/controller/ProductController.java
@@ -1,6 +1,5 @@
 package io.mipangg.querymarket.domain.product.controller;
 
-import io.mipangg.querymarket.domain.common.CursorPageResponse;
 import io.mipangg.querymarket.domain.common.ProductPageResponse;
 import io.mipangg.querymarket.domain.product.dto.ProductCreateRequest;
 import io.mipangg.querymarket.domain.product.dto.ProductDetailResponse;
